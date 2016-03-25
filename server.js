8dfb82a9c8bac3a97527e1baa314fb85f0c84e62
Lukas Lamm <lukas.lamm89@gmail.com>
2016-03-25 19:56:59 +0100

    Add test route

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', function(req, res, next) {
+	res.send('Hello World!');
+});
+
 app.post('/musicjson2abc', function(req, res, next) {
 	var data = req.body;
 	var abc = converter.convert2Abc(JSON.stringify(data));
